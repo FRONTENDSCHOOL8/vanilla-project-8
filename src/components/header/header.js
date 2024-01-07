febfe5d5b8754b2a233dd174cce59f585f646005
ryujinzz <kdo2240k@naver.com>
2024-01-07 21:48:38 +0900

    ✨ [feat] 윗 배너 광고 없애기 추가 구현 및 카테고리 버그 수정

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,9 @@
 import { getNode } from '../../lib/index.js';
 
-const category = getNode('.navigation');
+const category = getNode('.nav-category');
 const navMenu = getNode('.nav-menu-hide');
+const button = getNode('.header-ad-btn');
+const ad = getNode('.header-ad');
 
 function showNavMenu() {
   navMenu.style.opacity = 1;
@@ -11,5 +13,13 @@ function closeNavMenu() {
   navMenu.style.opacity = 0;
 }
 
+function closeAd() {
+  ad.style.display = 'none';
+  navMenu.style.top = '150px';
+}
+
 category.addEventListener('mouseover', showNavMenu);
+navMenu.addEventListener('mouseover', showNavMenu);
 category.addEventListener('mouseout', closeNavMenu);
+navMenu.addEventListener('mouseout', closeNavMenu);
+button.addEventListener('click', closeAd);
