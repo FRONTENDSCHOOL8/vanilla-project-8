fd4204e643af9f3c2521a7a79f86e85ab6012746
ryujinzz <kdo2240k@naver.com>
2024-01-08 21:05:07 +0900

    [test] 배포 테스트 상품페이지 추가, 경로변경

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         register: resolve(__dirname, 'src/pages/register/index.html'),
         login: resolve(__dirname, 'src/pages/login/index.html'),
         cart: resolve(__dirname, 'src/pages/cart/index.html'),
+        detail: resolve(__dirname, 'src/pages/detail/index.html'),
       },
     },
   },
