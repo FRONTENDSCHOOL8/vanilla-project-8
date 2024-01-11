a442c7654e54c8a7aabe09a3a9d9da0a4b47ac4c
Lulurem <smile71109@naver.com>
2024-01-11 09:41:15 +0900

    ✨ [feat] Karly Only, 한정수량 라벨 문구 출력 #40

--- src/pages/product/product.js
+++ src/pages/product/product.js
@@ -3,12 +3,14 @@ import {
   tiger,
   insertLast,
   comma,
+  getNode,
   getPbImageURL,
   getStorage,
   setStorage,
   setDocumentTitle,
 } from '/src/lib';
-import defaultAuthData from '/src/api/defaultAuthData';
+import pb from '/src/api/pocketbase';
+import defaultCartData from '../../api/defaultCartData';
 
 setDocumentTitle('검색 - 컬리');
 
@@ -16,33 +18,19 @@ async function renderProduct() {
   const response = await tiger.get(
     `${import.meta.env.VITE_PB_API}/collections/products/records`
   );
-
-  console.log(response);
   const userData = response.data.items;
-
-  const { isAuth } = await getStorage('auth');
 
   userData.forEach((item) => {
     const ratio = item.price * (item.discount * 0.01);
     const template = /* html */ `
             <li class="product-each">
-            <a href="${
-              !isAuth
-                ? '/src/pages/login/'
-                : `/src/pages/detail/index.html#${item.id}`
-            }">
-                <figure>
+              <button class="cart" type="button"></button>
+              <a href="${`/src/pages/detail/index.html#${item.id}`}">
                   <img
                     class="product-img"
                     src="${getPbImageURL(item)}"
                     alt=""
                   />
-                  <img
-                    class="cart-img"
-                    src="/images/product/cart-img.svg"
-                    alt=""
-                  />
-                </figure>
                 <div class="product-info">
                   <span class="delivery">샛별배송</span><br />
                   <span class="desc-1">${item.brand}</span><br />
@@ -54,14 +42,47 @@ async function renderProduct() {
                   <span class="desc-2">${item.description}</span><br />
                 </div>
                 <div class="label-container">
-                  <span class="karly-only">${item.karly_only}</span>
-                  <span class="limited">${item.limited}</span>
+                  <span class="karly-only ${item.karly_only ? '' : 'hidden'}">${
+                    item.karly_only ? 'Karly Only' : item.karly_only
+                  }</span>
+                  <span class="limited ${item.limited ? '' : 'hidden'}">${
+                    item.limited ? '한정수량' : item.limited
+                  }</span>
                 </div>
               </a>
             </li>
+
+            <script>
+              const karlyOnly = getNode('karly-only');
+              if (item.karly_only === false) {
+              karlyOnly.classList.add('hidden')}
+            </script>
             `;
+
     insertLast('.product-list', template);
   });
+
+  //
+
+  //
+
+  if (!localStorage.getItem('cart')) {
+    setStorage('cart', defaultCartData);
+  }
+
+  const hash = window.location.hash.slice(1);
+
+  const productData = await pb.collection('products').getOne(hash);
+  console.log(productData);
+
+  const cart = getNode('.cart');
+
+  function sendToCart() {
+    setStorage('cart', productData);
+    console.log('꽁꽁');
+  }
+
+  cart.addEventListener('click', sendToCart);
 }
 
 renderProduct();
