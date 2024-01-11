0d238df6b8b9a32b99e77ee1f286f156b21ba1fd
ryujinzz <kdo2240k@naver.com>
2024-01-11 10:48:21 +0900

    ✨ [feat] 로그아웃 레이아웃 조정 구현 완성

--- main.js
+++ main.js
@@ -1,23 +1,43 @@
 // import "/src/style/style.css";
 import pb from '/src/api/pocketbase.js';
 import defaultAuthData from '/src/api/defaultAuthData';
-import {
-  getNode,
-  getStorage,
-  insertLast,
-  deleteStorage,
-  setStorage,
-} from '/src/lib/';
+import { getNode, getStorage, setStorage } from '/src/lib/';
+
+function clearContents(node) {
+  if (typeof node === 'string') node = getNode(node);
+
+  if (node.nodeName === 'INPUT' || node.nodeName === 'TEXTAREA') {
+    node.value = '';
+    return;
+  }
+
+  node.textContent = '';
+}
 
 export async function mainjs() {
   if (localStorage.getItem('auth')) {
     const { isAuth, user } = await getStorage('auth');
     if (isAuth) {
-      const template = /* html */ `
-      <div class="userName">웰컴 ${user.name}님😘</div>
-      <button class="logout" type="button">로그아웃</button>
-    `;
       const headerRight = getNode('.header-right');
+      clearContents(headerRight);
+      const template = /* html */ `
+      <li>
+      <div class="userName">웰컴 ${user.name}님😘</div>
+          </li>
+          <li>
+            <span class="divider" aria-hidden="true">|</span>
+            <button class="logout" type="button">로그아웃</button>
+          </li>
+
+          <li>
+            <span class="divider" aria-hidden="true">|</span>
+            <a class="information" href="/"
+              >고객센터
+              <img src="/images/header/Icon_down.png" alt="" />
+            </a>
+          </li>
+    `;
+
       headerRight.insertAdjacentHTML('beforeend', template);
     }
   }
